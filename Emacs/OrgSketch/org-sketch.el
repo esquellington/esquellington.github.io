6db5e0fa1eea3b2dce08977ae65160da2e5e4c5d
Oscar Civit Flores <oscar.civit.flores@gmail.com>
2020-10-31 17:58:12 +0000

    added xournal++ support, yay!

--- Emacs/OrgSketch/org-sketch.el
+++ Emacs/OrgSketch/org-sketch.el
@@ -9,6 +9,48 @@
 ;;
 ;;; Code:
 
+;;--------------------------------
+;;---- TOOL = gnome-paint
+(defun org-sketch-tool-command ()
+  "Return tool-specific command."
+  "gnome-paint "
+)
+(defun org-sketch-tool-ext ()
+  "Return tool-specific file extension."
+  ".jpg"
+)
+(defun org-sketch-tool-template-file ()
+  "Return tool-specific template file."
+  ;;TODO CREATE IF not found
+  "template.jpg"
+)
+(defun org-sketch-tool-export-png ( input output )
+  "Export/Convert native INPUT to OUTPUT image."
+  (shell-command (concat "convert " input " " output " > /dev/null"))
+)
+
+;;--------------------------------
+;;---- TOOL = xournalpp
+(defun org-sketch-tool-command ()
+  "Return tool-specific command."
+  "./xournalpp-1.0.19-x86_64.AppImage "
+)
+(defun org-sketch-tool-ext ()
+  "Return tool-specific file extension."
+  ".xopp"
+)
+(defun org-sketch-tool-template-file ()
+  "Return tool-specific template file."
+  ;;TODO CREATE IF not found
+  "template.xopp"
+)
+(defun org-sketch-tool-export-png ( input output )
+  "Export/Convert native INPUT to OUTPUT image."
+  (shell-command (concat (org-sketch-tool-command) " " input " -i " output " > /dev/null"))
+)
+
+;;--------------------------------
+;; Interactive functions
 (defun org-sketch-insert ( skname )
   "Insert sketch SKNAME at point."
   (interactive "sSketch Name: ") ;"sXXXX" prompts user for string param SKNAME
@@ -18,21 +60,21 @@
     (when (or (not (file-exists-p skname_png))
               (yes-or-no-p "Sketch exists! Overwrite? "))
 
-      ;; Create sketch tool empty template file
-      (setq skname_ext (concat skname ".jpg"))
+      ;; Create sketch tool empty file from template
+      (setq skname_ext (concat skname (org-sketch-tool-ext)))
       (setq skname_timestamp (concat skname ".timestamp"))
-      (shell-command (concat "cp template.jpg " skname_ext " > /dev/null"))
+      (shell-command (concat "cp " (org-sketch-tool-template-file) " " skname_ext " > /dev/null"))
 
       ;; Create timestamp file afterwards, to detect if skname_ext is overwritten by tool
       (shell-command (concat "touch " skname_timestamp " > /dev/null"))
 
       ;; Open sketch tool on empty file and wait for close/exit
-      (shell-command (concat "gnome-paint " skname_ext " > /dev/null"))
+      (shell-command (concat (org-sketch-tool-command) skname_ext " > /dev/null"))
 
       ;; Check if tool file was saved, continue processing if it was, delete and exit if not
       (when (file-newer-than-file-p skname_ext skname_timestamp)
-        ;; Convert to .PNG
-        (shell-command (concat "convert " skname_ext " " skname_png " > /dev/null"))
+        ;; Export to .PNG
+        (org-sketch-tool-export-png skname_ext skname_png)
 
         ;; Trim empty space
         (shell-command (concat "convert -trim " skname_png " " skname_png " > /dev/null"))
