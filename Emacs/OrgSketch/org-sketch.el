fbdad27f783b064cb56dae1c2bf2911ff56ae90a
Oscar Civit Flores <oscar.civit.flores@gmail.com>
2020-11-01 16:16:10 +0000

    added Inkscape support, no template generation yet

--- Emacs/OrgSketch/org-sketch.el
+++ Emacs/OrgSketch/org-sketch.el
@@ -39,11 +39,11 @@
   "Default sketch tool."
   :group 'org-sketch
   :type '(choice (const :tag "Best Available" nil)
-                 (const :tag "gnome-paint" gnome-paint)
-                 (const :tag "xournal++" xournalpp)
                  (const :tag "gimp" gimp)
-                 ;;(const :tag "inkscape" inkscape)
+                 (const :tag "gnome-paint" gnome-paint)
+                 (const :tag "inkscape" inkscape)
                  (const :tag "mspaint" mspaint) ;TODO WINDOWS ONLY, try to add conditionally?
+                 (const :tag "xournal++" xournalpp)
                  ))
 
 ;;--------------------------------
@@ -79,16 +79,16 @@
   "Return tool-specific template file."
   ;; Check empty template, create blank .PNG if none
   (let (template_file)
-    (setq template_file (concat org-sketch-output-dir "/org-sketch-template.png"))
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template--GP.png"))
     (when (not (file-exists-p template_file))
       (shell-command (concat "convert -size 900x450 xc:white " template_file org-sketch-commandline-null-sink)))
     template_file))
 (defun org-sketch-tool-export-png--GP ( input output )
-  "Export/Convert native INPUT to OUTPUT image."
+  "Export/Convert native INPUT to OUTPUT .PNG image."
   (shell-command (concat "convert " input " " output org-sketch-commandline-null-sink)))
 
 ;;--------------------------------
-;; TOOL: MSPaint
+;; TOOL: mspaint
 ;;--------------------------------
 (defun org-sketch-tool-command--MSP ()
   "Return tool-specific command."
@@ -100,12 +100,12 @@
   "Return tool-specific template file."
   ;; Check empty template, create blank .PNG if none
   (let (template_file)
-    (setq template_file (concat org-sketch-output-dir "/org-sketch-template.png"))
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template--MSP.png"))
     (when (not (file-exists-p template_file))
       (shell-command (concat "convert -size 900x450 xc:white " template_file org-sketch-commandline-null-sink)))
     template_file))
 (defun org-sketch-tool-export-png--MSP ( input output )
-  "Export/Convert native INPUT to OUTPUT image."
+  "Export/Convert native INPUT to OUTPUT .PNG image."
   (shell-command (concat "convert " input " " output org-sketch-commandline-null-sink)))
 
 ;;--------------------------------
@@ -122,7 +122,7 @@
   "Return tool-specific template file."
   ;; Check empty template, create if not available
   (let (template_file)
-    (setq template_file (concat org-sketch-output-dir "/org-sketch-template.xopp"))
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template--XPP.xopp"))
     (when (not (file-exists-p template_file))
       ;; The .xopp files are XML compressed with gzip, so I
       ;; uncompressed an empty canvas .xopp and pasted the XML here
@@ -145,33 +145,76 @@
     ;; Return
     template_file))
 (defun org-sketch-tool-export-png--XPP ( input output )
-  "Export/Convert native INPUT to OUTPUT image."
+  "Export/Convert native INPUT to OUTPUT .PNG image."
+  ;; xournalpp -i exports to .PNG
   (shell-command (concat (org-sketch-tool-command--XPP) " " input " -i " output org-sketch-commandline-null-sink)))
 
 ;;--------------------------------
-;; TOOL: GIMP
+;; TOOL: inkscape
 ;;--------------------------------
-;; (defun org-sketch-tool-command--GIMP ()
-;;   "Return tool-specific command."
-;;   "gimp")
-;; (defun org-sketch-tool-ext--GIMP ()
-;;   "Return tool-specific file extension."
-;;   ".xcf")
-;; (defun org-sketch-tool-template-file--GIMP ()
-;;   "Return tool-specific template file."
-;;   ;; Check empty template, create blank .XCF if none
-;;   (let (template_file)
-;;     (setq template_file_png (concat org-sketch-output-dir "/org-sketch-template-GIMP.png"))
-;;     (setq template_file (concat org-sketch-output-dir "/org-sketch-template.xcf"))
-;;     (when (not (file-exists-p template_file))
-;;       ;; Create blank .PNG and convert to .XCF (ImageMagik cannot create .XCF directly)
-;;       (shell-command (concat "convert -size 900x450 xc:white " template_file_png org-sketch-commandline-null-sink))
-;;       (shell-command (concat "convert " template_file_png " " template_file org-sketch-commandline-null-sink)))
-;;     template_file))
-;; (defun org-sketch-tool-export-png--GIMP ( input output )
-;;   "Export/Convert native INPUT to OUTPUT image."
-;;   ;; ImageMagik seems supports convertint to/from .XCF
-;;   (shell-command (concat "convert " input " " output org-sketch-commandline-null-sink)))
+(defun org-sketch-tool-command--INK ()
+  "Return tool-specific command."
+  "inkscape")
+;;  "./xournalpp-1.0.19-x86_64.AppImage ")
+(defun org-sketch-tool-ext--INK ()
+  "Return tool-specific file extension."
+  ".svg")
+(defun org-sketch-tool-template-file--INK ()
+  "Return tool-specific template file."
+  ;; Check empty template, create if not available
+  (let (template_file)
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template--INK.svg"))
+    ;; TODO GEN .svg on the fly!?
+    ;; (when (not (file-exists-p template_file))
+    ;;   ;; The .xopp files are XML compressed with gzip, so I
+    ;;   ;; uncompressed an empty canvas .xopp and pasted the XML here
+    ;;   ;; TODO could parametrize template sizes and colors, but better
+    ;;   ;; keep it simple and have a unique base template and
+    ;;   ;; resize/modify individual outputs instead
+    ;;   (shell-command (concat "echo '"
+    ;;                          "<?xml version=\"1.0\" standalone=\"no\"?>"
+    ;;                          "<xournal creator=\"Xournal++ 1.0.19\" fileversion=\"4\">"
+    ;;                          "<title>Xournal++ document - see https://github.com/xournalpp/xournalpp</title>"
+    ;;                          "<preview/>"
+    ;;                          "<page width=\"900.0\" height=\"450.0\">" ;;16/9
+    ;;                          "<background type=\"solid\" color=\"#ffffffff\" style=\"plain\"/>"
+    ;;                          "<layer/>"
+    ;;                          "</page>"
+    ;;                          "</xournal>"
+    ;;                          "' | gzip > "
+    ;;                          template_file ))
+    ;;   )
+    ;; Return
+    template_file))
+(defun org-sketch-tool-export-png--INK ( input output )
+  "Export/Convert native INPUT to OUTPUT .PNG image."
+  ;; inkscape -e exports to .PNG
+  (shell-command (concat (org-sketch-tool-command--INK) " " input " -e " output org-sketch-commandline-null-sink)))
+
+;;--------------------------------
+;; TOOL: gimp
+;;--------------------------------
+(defun org-sketch-tool-command--GIMP ()
+  "Return tool-specific command."
+  "gimp")
+(defun org-sketch-tool-ext--GIMP ()
+  "Return tool-specific file extension."
+  ".xcf")
+(defun org-sketch-tool-template-file--GIMP ()
+  "Return tool-specific template file."
+  ;; Check empty template, create blank .XCF if none
+  (let (template_file)
+    (setq template_file_png (concat org-sketch-output-dir "/org-sketch-template-GIMP.png"))
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template--GIMP.xcf"))
+    (when (not (file-exists-p template_file))
+      ;; Create blank .PNG and convert to .XCF (ImageMagik cannot create .XCF directly)
+      (shell-command (concat "convert -size 900x450 xc:white " template_file_png org-sketch-commandline-null-sink))
+      (shell-command (concat "convert " template_file_png " " template_file org-sketch-commandline-null-sink)))
+    template_file))
+(defun org-sketch-tool-export-png--GIMP ( input output )
+  "Export/Convert native INPUT to OUTPUT .PNG image."
+  ;; ImageMagik seems supports converting to/from .XCF
+  (shell-command (concat "convert " input " " output org-sketch-commandline-null-sink)))
 
 ;;--------------------------------
 ;; Interactive functions
@@ -187,12 +230,12 @@
 
   ;; Select tool
   ;; TODO Try to do this only once on startup or similar
-  (cond ((eq org-sketch-tool 'xournalpp)
-         (fset 'org-sketch-tool-command 'org-sketch-tool-command--XPP)
-         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--XPP)
-         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--XPP)
-         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--XPP)
-         ;;(message "XOURNAL++")
+  (cond ((eq org-sketch-tool 'gimp)
+         (fset 'org-sketch-tool-command 'org-sketch-tool-command--GIMP)
+         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--GIMP)
+         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--GIMP)
+         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--GIMP)
+         ;;(message "GIMP")
          )
         ((eq org-sketch-tool 'gnome-paint)
          (fset 'org-sketch-tool-command 'org-sketch-tool-command--GP)
@@ -201,13 +244,20 @@
          (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--GP)
          ;;(message "GNOME-PAINT")
          )
-        ;; ((eq org-sketch-tool 'gimp)
-        ;;  (fset 'org-sketch-tool-command 'org-sketch-tool-command--GIMP)
-        ;;  (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--GIMP)
-        ;;  (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--GIMP)
-        ;;  (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--GIMP)
-        ;;  ;;(message "GIMP")
-        ;;  )
+        ((eq org-sketch-tool 'inkscape)
+         (fset 'org-sketch-tool-command 'org-sketch-tool-command--INK)
+         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--INK)
+         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--INK)
+         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--INK)
+         ;;(message "INKSCAPE")
+         )
+        ((eq org-sketch-tool 'xournalpp)
+         (fset 'org-sketch-tool-command 'org-sketch-tool-command--XPP)
+         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--XPP)
+         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--XPP)
+         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--XPP)
+         ;;(message "XOURNAL++")
+         )
         (t ;;default covers nil (best available) case too, by now
          (fset 'org-sketch-tool-command 'org-sketch-tool-command--GP)
          (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--GP)
