c5533c95614147715b53bf66c89a173f401a32c0
Oscar Civit Flores <oscar.civit.flores@gmail.com>
2020-11-01 14:57:35 +0000

    tool select (GP,XPP) and corrected temp file dir

--- Emacs/OrgSketch/org-sketch.el
+++ Emacs/OrgSketch/org-sketch.el
@@ -31,6 +31,16 @@
   :group 'org-sketch
   :type 'integer)
 
+(defcustom org-sketch-tool nil
+  "Default sketch tool."
+  :group 'org-sketch
+  :type '(choice (const :tag "Best Available" nil)
+                 (const :tag "gnome-paint" gnome-paint)
+                 (const :tag "xournal++" xournalpp)
+                 ;;(const :tag "gimp" gimp)
+                 ;;(const :tag "inkscape" inkscape)
+                 ))
+
 ;;--------------------------------
 ;; Basic helpers
 ;;--------------------------------
@@ -46,69 +56,63 @@
 ;;--------------------------------
 ;; TOOL: gnome-paint
 ;;--------------------------------
-(defun org-sketch-tool-command ()
+(defun org-sketch-tool-command--GP ()
   "Return tool-specific command."
-  "gnome-paint "
-)
-(defun org-sketch-tool-ext ()
+  "gnome-paint ")
+(defun org-sketch-tool-ext--GP ()
   "Return tool-specific file extension."
-  ".png"
-)
-(defun org-sketch-tool-template-file ()
+  ".png")
+(defun org-sketch-tool-template-file--GP ()
   "Return tool-specific template file."
-  ;; Check empty template, create if not available
-  (when (not (file-exists-p "template_genearted.png"))
-    ;; Create blank .PNG
-    (shell-command (concat "convert -size 900x450 xc:white org-sketch-template.png > /dev/null")))
-  "org-sketch-template.png"
-)
-(defun org-sketch-tool-export-png ( input output )
+  ;; Check empty template, create blank .PNG if none
+  (let (template_file)
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template.png"))
+    (when (not (file-exists-p template_file))
+      (shell-command (concat "convert -size 900x450 xc:white " template_file " > /dev/null")))
+    template_file))
+(defun org-sketch-tool-export-png--GP ( input output )
   "Export/Convert native INPUT to OUTPUT image."
-  (shell-command (concat "convert " input " " output " > /dev/null"))
-)
+  (shell-command (concat "convert " input " " output " > /dev/null")))
 
 ;;--------------------------------
 ;; TOOL: xournalpp
 ;;--------------------------------
-;;TODO SOME OF THESE SHOULD BE defvar and customizable I guess
-;; (defun org-sketch-tool-command ()
-;;   "Return tool-specific command."
-;;TODO PROPER GLOBAL PATH ~/Escriptori/esquellington/ext/Xournal/xournalpp-1.0.19-x86_64.AppImage
-;;   "./xournalpp-1.0.19-x86_64.AppImage "
-;; )
-;; (defun org-sketch-tool-ext ()
-;;   "Return tool-specific file extension."
-;;   ".xopp"
-;; )
-;; (defun org-sketch-tool-template-file ()
-;;   "Return tool-specific template file."
-;;   ;; Check empty template, create if not available
-;;   (when (not (file-exists-p "template_genearted.xopp"))
-;;     ;; The .xopp files are XML compressed with gzip, so I
-;;     ;; uncompressed an empty canvas .xopp and pasted the XML here
-;;     ;; TODO could parametrize template sizes and colors, but better
-;;     ;; keep it simple and have a unique base template and
-;;     ;; resize/modify individual outputs instead
-;;     (shell-command (concat "echo '"
-;;                            "<?xml version=\"1.0\" standalone=\"no\"?>"
-;;                            "<xournal creator=\"Xournal++ 1.0.19\" fileversion=\"4\">"
-;;                            "<title>Xournal++ document - see https://github.com/xournalpp/xournalpp</title>"
-;;                            "<preview/>"
-;;                            "<page width=\"900.0\" height=\"450.0\">" ;;16/9
-;;                            "<background type=\"solid\" color=\"#ffffffff\" style=\"plain\"/>"
-;;                            "<layer/>"
-;;                            "</page>"
-;;                            "</xournal>"
-;;                            "' | gzip > org-sketch-template.xopp" ))
-;;       )
-;;   ;; Return
-;;   "org-sketch-template.xopp"
-;;   )
-
-;; (defun org-sketch-tool-export-png ( input output )
-;;   "Export/Convert native INPUT to OUTPUT image."
-;;   (shell-command (concat (org-sketch-tool-command) " " input " -i " output " > /dev/null"))
-;;)
+(defun org-sketch-tool-command--XPP ()
+  "Return tool-specific command."
+  "~/Escriptori/esquellington/ext/bin/xournalpp-1.0.19-x86_64.AppImage ")
+;;  "./xournalpp-1.0.19-x86_64.AppImage ")
+(defun org-sketch-tool-ext--XPP ()
+  "Return tool-specific file extension."
+  ".xopp")
+(defun org-sketch-tool-template-file--XPP ()
+  "Return tool-specific template file."
+  ;; Check empty template, create if not available
+  (let (template_file)
+    (setq template_file (concat org-sketch-output-dir "/org-sketch-template.xopp"))
+    (when (not (file-exists-p template_file))
+      ;; The .xopp files are XML compressed with gzip, so I
+      ;; uncompressed an empty canvas .xopp and pasted the XML here
+      ;; TODO could parametrize template sizes and colors, but better
+      ;; keep it simple and have a unique base template and
+      ;; resize/modify individual outputs instead
+      (shell-command (concat "echo '"
+                             "<?xml version=\"1.0\" standalone=\"no\"?>"
+                             "<xournal creator=\"Xournal++ 1.0.19\" fileversion=\"4\">"
+                             "<title>Xournal++ document - see https://github.com/xournalpp/xournalpp</title>"
+                             "<preview/>"
+                             "<page width=\"900.0\" height=\"450.0\">" ;;16/9
+                             "<background type=\"solid\" color=\"#ffffffff\" style=\"plain\"/>"
+                             "<layer/>"
+                             "</page>"
+                             "</xournal>"
+                             "' | gzip > "
+                             template_file ))
+      )
+    ;; Return
+    template_file))
+(defun org-sketch-tool-export-png--XPP ( input output )
+  "Export/Convert native INPUT to OUTPUT image."
+  (shell-command (concat (org-sketch-tool-command) " " input " -i " output " > /dev/null")))
 
 ;;--------------------------------
 ;; Interactive functions
@@ -121,6 +125,27 @@
   (when (string-empty-p skname) (setq skname "UNNAMED_SKETCH")) ;TODO find unique name
   (when (eq width nil) (setq width (org-sketch-output-width)))
   (when (eq height nil) (setq height (org-sketch-output-height)))
+
+  ;; Select tool
+  ;; TODO Try to do this only once on startup or similar
+  (cond ((eq org-sketch-tool 'xournalpp)
+         (fset 'org-sketch-tool-command 'org-sketch-tool-command--XPP)
+         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--XPP)
+         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--XPP)
+         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--XPP)
+         (message "XOURNAL++"))
+        ((eq org-sketch-tool 'gnome-paint)
+         (fset 'org-sketch-tool-command 'org-sketch-tool-command--GP)
+         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--GP)
+         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--GP)
+         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--GP)
+         (message "GNOME-PAINT"))
+        (t ;;default covers nil (best available) case too, by now
+         (fset 'org-sketch-tool-command 'org-sketch-tool-command--GP)
+         (fset 'org-sketch-tool-ext 'org-sketch-tool-ext--GP)
+         (fset 'org-sketch-tool-template-file 'org-sketch-tool-template-file--GP)
+         (fset 'org-sketch-tool-export-png 'org-sketch-tool-export-png--GP)
+         (message "DEFAULT")))
 
   (let (skname_tmp_ext skname_png skname_timestamp)
 
@@ -135,8 +160,8 @@
               (yes-or-no-p "Sketch exists! Overwrite? "))
 
       ;; Create sketch tool empty file from template
-      (setq skname_tmp_ext (concat org-sketch-output-dir skname "_tmp" (org-sketch-tool-ext)))
-      (setq skname_timestamp (concat org-sketch-output-dir skname ".timestamp"))
+      (setq skname_tmp_ext (concat org-sketch-output-dir "/" skname "_tmp" (org-sketch-tool-ext)))
+      (setq skname_timestamp (concat org-sketch-output-dir "/" skname ".timestamp"))
       (shell-command (concat "cp " (org-sketch-tool-template-file) " " skname_tmp_ext " > /dev/null"))
 
       ;; Create timestamp file afterwards, to detect if skname_tmp_ext is overwritten by tool
@@ -167,7 +192,6 @@
         ;; C-v C-x). To display image links with description a
         ;; non-nil prefix argument must be passed (C-u C-c C-v C-x)
         (org-insert-link nil (concat "file:" skname_png) nil)
-        ;(message (concat "inserted sketch " skname) )
         )
 
       ;; Delete temp
