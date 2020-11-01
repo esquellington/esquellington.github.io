269863d812fd4b0b45d111c6f35ac761abae79af
Oscar Civit Flores <oscar.civit.flores@gmail.com>
2020-11-01 00:57:17 +0000

    set gnome-paint as default tool, added auto template gen for GP

--- Emacs/OrgSketch/org-sketch.el
+++ Emacs/OrgSketch/org-sketch.el
@@ -17,12 +17,15 @@
 )
 (defun org-sketch-tool-ext ()
   "Return tool-specific file extension."
-  ".jpg"
+  ".png"
 )
 (defun org-sketch-tool-template-file ()
   "Return tool-specific template file."
-  ;;TODO empty .JPG with correct sizes if not found
-  "template.jpg"
+   ;; Check empty template, create if not available
+  (when (not (file-exists-p "template_genearted.xopp"))
+    ;; Create blank .PNG
+    (shell-command (concat "convert -size 900x450 xc:white org-sketch-template.png > /dev/null")))
+  "org-sketch-template.png"
 )
 (defun org-sketch-tool-export-png ( input output )
   "Export/Convert native INPUT to OUTPUT image."
@@ -32,42 +35,42 @@
 ;;--------------------------------
 ;;---- TOOL = xournalpp
 ;;TODO SOME OF THESE SHOULD BE defvar and customizable I guess
-(defun org-sketch-tool-command ()
-  "Return tool-specific command."
-  "./xournalpp-1.0.19-x86_64.AppImage "
-)
-(defun org-sketch-tool-ext ()
-  "Return tool-specific file extension."
-  ".xopp"
-)
-(defun org-sketch-tool-template-file ()
-  "Return tool-specific template file."
-  ;; Check empty template, create if not available
-  (when (not (file-exists-p "template_genearted.xopp"))
-    ;; Empty .xopp is XML compressed with gzip
-    ;; TODO could parametrize template sizes and colors, but better
-    ;; keep it simple and have a unique base template and
-    ;; resize/modify individual outputs instead
-    (shell-command (concat "echo '"
-                           "<?xml version=\"1.0\" standalone=\"no\"?>"
-                           "<xournal creator=\"Xournal++ 1.0.19\" fileversion=\"4\">"
-                           "<title>Xournal++ document - see https://github.com/xournalpp/xournalpp</title>"
-                           "<preview/>"
-                           "<page width=\"900.0\" height=\"450.0\">" ;;16/9
-                           "<background type=\"solid\" color=\"#ffffffff\" style=\"plain\"/>"
-                           "<layer/>"
-                           "</page>"
-                           "</xournal>"
-                           "' | gzip > template_generated.xopp" ))
-      )
-  ;; Return
-  "template_generated.xopp"
-  )
+;; (defun org-sketch-tool-command ()
+;;   "Return tool-specific command."
+;;   "./xournalpp-1.0.19-x86_64.AppImage "
+;; )
+;; (defun org-sketch-tool-ext ()
+;;   "Return tool-specific file extension."
+;;   ".xopp"
+;; )
+;; (defun org-sketch-tool-template-file ()
+;;   "Return tool-specific template file."
+;;   ;; Check empty template, create if not available
+;;   (when (not (file-exists-p "template_genearted.xopp"))
+;;     ;; Empty .xopp is XML compressed with gzip
+;;     ;; TODO could parametrize template sizes and colors, but better
+;;     ;; keep it simple and have a unique base template and
+;;     ;; resize/modify individual outputs instead
+;;     (shell-command (concat "echo '"
+;;                            "<?xml version=\"1.0\" standalone=\"no\"?>"
+;;                            "<xournal creator=\"Xournal++ 1.0.19\" fileversion=\"4\">"
+;;                            "<title>Xournal++ document - see https://github.com/xournalpp/xournalpp</title>"
+;;                            "<preview/>"
+;;                            "<page width=\"900.0\" height=\"450.0\">" ;;16/9
+;;                            "<background type=\"solid\" color=\"#ffffffff\" style=\"plain\"/>"
+;;                            "<layer/>"
+;;                            "</page>"
+;;                            "</xournal>"
+;;                            "' | gzip > org-sketch-template.xopp" ))
+;;       )
+;;   ;; Return
+;;   "org-sketch-template.xopp"
+;;   )
 
-(defun org-sketch-tool-export-png ( input output )
-  "Export/Convert native INPUT to OUTPUT image."
-  (shell-command (concat (org-sketch-tool-command) " " input " -i " output " > /dev/null"))
-)
+;; (defun org-sketch-tool-export-png ( input output )
+;;   "Export/Convert native INPUT to OUTPUT image."
+;;   (shell-command (concat (org-sketch-tool-command) " " input " -i " output " > /dev/null"))
+;;)
 
 ;;--------------------------------
 ;; Interactive functions
@@ -80,7 +83,7 @@
   (when (eq width nil) (setq width 600))
   (when (eq height nil) (setq height 400))
 
-  (let (skname_ext skname_png skname_timestamp)
+  (let (skname_tmp_ext skname_png skname_timestamp)
 
     (setq skname_png (concat skname ".png"))
 
@@ -89,21 +92,21 @@
               (yes-or-no-p "Sketch exists! Overwrite? "))
 
       ;; Create sketch tool empty file from template
-      (setq skname_ext (concat skname (org-sketch-tool-ext)))
+      (setq skname_tmp_ext (concat skname "_tmp" (org-sketch-tool-ext)))
       (setq skname_timestamp (concat skname ".timestamp"))
-      (shell-command (concat "cp " (org-sketch-tool-template-file) " " skname_ext " > /dev/null"))
+      (shell-command (concat "cp " (org-sketch-tool-template-file) " " skname_tmp_ext " > /dev/null"))
 
-      ;; Create timestamp file afterwards, to detect if skname_ext is overwritten by tool
+      ;; Create timestamp file afterwards, to detect if skname_tmp_ext is overwritten by tool
       (shell-command (concat "touch " skname_timestamp " > /dev/null"))
 
       ;; Open sketch tool on empty file and wait for close/exit
-      (shell-command (concat (org-sketch-tool-command) skname_ext " > /dev/null"))
+      (shell-command (concat (org-sketch-tool-command) skname_tmp_ext " > /dev/null"))
 
       ;; Check if tool file was saved, continue processing if it was, delete and exit if not
-      (when (file-newer-than-file-p skname_ext skname_timestamp)
+      (when (file-newer-than-file-p skname_tmp_ext skname_timestamp)
 
         ;; Export to .PNG
-        (org-sketch-tool-export-png skname_ext skname_png)
+        (org-sketch-tool-export-png skname_tmp_ext skname_png)
 
         ;; Trim empty space and resize
         (shell-command (concat "convert -trim"
@@ -124,13 +127,10 @@
         )
 
       ;; Delete temp
-      (shell-command (concat "rm " skname_ext " " skname_timestamp " > /dev/null"))
+      (shell-command (concat "rm " skname_tmp_ext " " skname_timestamp " > /dev/null"))
       )
     )
   )
-
-;; TODO command, should be org-only or similar, define in init.el
-(global-set-key (kbd "C-c s") 'org-sketch-insert)
 
 (provide 'org-sketch)
 ;;; org-sketch.el ends here
